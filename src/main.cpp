c6848bba18e8e58c37f3f2948667f6413b323c84
SrNazaraK <59491741+SrNazaraK@users.noreply.github.com>
2025-11-28 23:24:32 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -31,3 +31,94 @@ int main() {
     
     cout << "\nPresione Enter para continuar...";
     cin.get();
+    
+    int opcion;
+    do {
+        mostrarMenu();
+        cin >> opcion;
+        
+        switch(opcion) {
+            case 1: {
+                grafo.mostrarGrafo();
+                break;
+            }
+            
+            case 2: {
+                grafo.mostrarMatriz();
+                break;
+            }
+            
+            case 3: {
+                int origen, destino, peso;
+                cout << "\nVertice origen: ";
+                cin >> origen;
+                cout << "Vertice destino: ";
+                cin >> destino;
+                cout << "Peso: ";
+                cin >> peso;
+                grafo.agregarArista(origen, destino, peso);
+                break;
+            }
+            
+            case 4: {
+                int inicio;
+                cout << "\nVertice de inicio: ";
+                cin >> inicio;
+                grafo.BFS(inicio);
+                break;
+            }
+            
+            case 5: {
+                int inicio;
+                cout << "\nVertice de inicio: ";
+                cin >> inicio;
+                grafo.DFS(inicio);
+                break;
+            }
+            
+            case 6: {
+                int origen, destino;
+                cout << "\nVertice origen: ";
+                cin >> origen;
+                cout << "Vertice destino: ";
+                cin >> destino;
+                grafo.dijkstra(origen, destino);
+                break;
+            }
+            
+            case 7: {
+                if (grafo.esConexo()) {
+                    cout << "\nEl grafo SI es conexo" << endl;
+                } else {
+                    cout << "\nEl grafo NO es conexo" << endl;
+                }
+                break;
+            }
+            
+            case 8: {
+                cout << "\n==INFORMACION==" << endl;
+                cout << "Numero de aristas: " << grafo.numeroAristas() << endl;
+                cout << "Conexo: " << (grafo.esConexo() ? "Si" : "No") << endl;
+                break;
+            }
+            
+            case 0: {
+                cout << "\nGracias por usar la libreria" << endl;
+                break;
+            }
+            
+            default: {
+                cout << "\nOpcion invalida" << endl;
+            }
+        }
+        
+        if (opcion != 0) {
+            cout << "\nPresiona enter para seguir.";
+            cin.ignore();
+            cin.get();
+        }
+        
+    } while(opcion != 0);
+    
+    return 0;
+}
